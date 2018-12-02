23d6b624b391e39251085d7216c47593acbd9f6a
Fabien Townsend <fabientownsend@gmail.com>
2018-12-02 15:05:39 +0000

    Adds compile and read write file tips

---
+++ other/compile.sh
@@ -0,0 +1,2 @@
+#from book g++ -std=c++11 -O2 -Wall test.cpp -o test
+g++ -std=c++11 -O2 -Wall main.cpp -o test
---
+++ other/read_write_to_file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // freopen("output.txt", "r", stdin); // read from file
+  freopen("output.txt", "w", stdout); // write to file when cout
+
+  int a, b;
+  string c;
+  cin >> a >> b >> c;
+  cout << a;
+  cout << b;
+  cout << c;
+}
